5d43f9c6bf100cc3d22c31a9c26390bffa66763e
unknown <satwik0103@gmail.com>
2022-03-27 22:30:51 +0530

    FinalYearProject Upload 1

---
+++ StudyBuddy/StudyBuddy/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const PORT=5000
+const path = require('path');
+app.use(express.static(__dirname + '/assets'))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+app.listen(PORT, () => console.log(` Started and listening on port ${PORT}!`));
